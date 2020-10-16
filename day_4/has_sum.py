572d246c6527cd2ea02fb8d14de5f2d783bf9627
Александр Придача <alex322p9@gmail.com>
2020-10-16 16:26:27 +0300

    Task 1, day 4: small refactoring

--- day_4/has_sum.py
+++ day_4/has_sum.py
@@ -1,10 +1,10 @@
 
 def has_sum(nums, target):
-    lookup = []
+    lookup = set()
     for i, num in enumerate(nums):
         if target - num in lookup:
             return True
-        lookup.append(num)
+        lookup.add(num)
 
 
 list_of_numbers = [1, 2, 4, 7, 2, 5, 25, 64, 4, 5, 6, 1, 1, 1, 1, 1]
