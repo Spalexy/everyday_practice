4557165d46ced2444f463396278f477e8655a3ce
Александр Придача <alex322p9@gmail.com>
2020-11-06 23:16:17 +0300

    Day 24: list_verifier

---
+++ day_24/list_verifier.py
@@ -0,0 +1,23 @@
+
+def get_list_from_int(integer: int) -> list:
+    list_to_return = []
+    while integer > 0:
+        list_to_return.append(integer % 10)
+        integer = integer // 10
+        list_to_return = list_to_return[::-1]
+    return list_to_return
+
+
+def verify_list_of_lists(verification_mask: int, list_to_verifying) -> bool:
+    verification_mask = get_list_from_int(verification_mask)
+    if len(verification_mask) == len(list_to_verifying):
+        result = True
+        for i in range(len(list_to_verifying)):
+            if len(list_to_verifying[i]) != verification_mask[i]:
+                result = False
+    else:
+        result = False
+    return result
+
+
+print(verify_list_of_lists(122, [[1], ['apple', 4], [5, 6.4]]))
